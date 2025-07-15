eb82742064ecb41959b458d600f015b4e86e025d
dohwi <dohwe0978@gmail.com>
2025-07-15 19:41:39 +0900

    feat :: exception

---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/user/exception/PasswordMissMatch.kt
@@ -0,0 +1,9 @@
+package com.example.debatematch.domain.user.exception
+
+import com.example.debatematch.global.error.exception.CustomException
+import com.example.debatematch.global.error.exception.ErrorCode
+
+object PasswordMissMatch: CustomException(
+    ErrorCode.PASSWORD_MISS_MATCH
+) {
+}
