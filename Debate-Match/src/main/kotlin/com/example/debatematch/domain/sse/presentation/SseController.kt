1c2c349fe0689dc3121cff64e803627481ce8373
dohwi <dohwe0978@gmail.com>
2025-07-16 10:46:38 +0900

    feat :: sse

---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/sse/presentation/SseController.kt
@@ -0,0 +1,20 @@
+package com.example.debatematch.domain.sse.presentation
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import com.example.debatematch.domain.sse.service.SseConnectService
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+@RequestMapping("/sse")
+class SseController(
+    private val sseConnectService: SseConnectService
+) {
+    @GetMapping(path = ["/connect"], produces= [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun connect(@RequestParam debateId: UUID, side: DebateSide)=sseConnectService.execute(debateId,side)
+
+}
---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/sse/service/SseConnectService.kt
@@ -0,0 +1,18 @@
+package com.example.debatematch.domain.sse.service
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import com.example.debatematch.domain.debate.facade.DebateFacade
+import org.springframework.stereotype.Service
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import java.util.*
+
+@Service
+class SseConnectService(
+    private val debateFacade: DebateFacade
+) {
+    fun execute(debateId: UUID, side: DebateSide): SseEmitter {
+        val emitter = debateFacade.createEmitter(debateId, side)
+        emitter.send("HelloWorld")
+        return emitter
+    }
+}
