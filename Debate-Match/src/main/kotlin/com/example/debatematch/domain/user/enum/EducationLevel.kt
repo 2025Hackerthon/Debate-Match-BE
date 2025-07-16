57042b658c60983a193e43212d7d0dd9d59617f3
dohwi <dohwe0978@gmail.com>
2025-07-16 11:02:55 +0900

    feat :: value 삭제

--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/user/enum/EducationLevel.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/user/enum/EducationLevel.kt
@@ -1,12 +1,10 @@
 package com.example.debatematch.domain.user.enum
 
-enum class EducationLevel(val displayName: String) {
-    ELEMENTARY_SCHOOL("초등학교 졸업"),
-    MIDDLE_SCHOOL("중학교 졸업"),
-    HIGH_SCHOOL("고등학교 졸업"),
-    UNIVERSITY("대학교 졸업"),
-    GRADUATE_SCHOOL("대학원 졸업"),
-    NONE("없음");
-
-    override fun toString(): String = displayName
+enum class EducationLevel() {
+    ELEMENTARY_SCHOOL,
+    MIDDLE_SCHOOL,
+    HIGH_SCHOOL,
+    UNIVERSITY,
+    GRADUATE_SCHOOL,
+    NONE
 }
