52eeaf8d282297c828c8a155ae2cb54069ae8d5d
dohwi <dohwe0978@gmail.com>
2025-07-16 11:22:07 +0900

    feat :: 로직 수정

--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/reaction/serivce/ReactionService.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/reaction/serivce/ReactionService.kt
@@ -17,12 +17,22 @@ class ReactionService(
     private val debateRepository: DebateRepository
 ) {
     @Transactional
-    fun execute(request: ReactionRequest){
+    fun execute(request: ReactionRequest) {
         val user = userFacade.currentUser()
         val debate = debateRepository.findById(request.debateId).orElseThrow()
-        if(debate.status != DebateStatus.DONE) throw InvalidDebateReactionException
+        if (debate.status != DebateStatus.DONE) throw InvalidDebateReactionException
 
-        val reaction = if (reactionRepository.existsByDebateIdAndUserId(request.debateId, user.id!!)) reactionRepository.findByDebateIdAndUserId(request.debateId, user.id!!) else Reaction(debateId = request.debateId, userId = user.id!!, request.reaction)
-        reaction!!.update(request.reaction)
+        val reaction = if (reactionRepository.existsByDebateIdAndUserId(
+                request.debateId,
+                user.id!!
+            )
+        ) reactionRepository.findByDebateIdAndUserId(request.debateId, user.id!!) else reactionRepository.save(
+            Reaction(
+                debateId = request.debateId,
+                userId = user.id!!,
+                request.reaction
+            )
+        )
+        reaction!!.reaction = request.reaction
     }
 }
