a9b8116cee4ab5283bcf72ed8bb5e97708aeb062
dohwi <dohwe0978@gmail.com>
2025-07-16 10:48:43 +0900

    feat :: persistence debate tag

--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/debatetag/DebateTag.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debatetag/DebateTag.kt
@@ -1,13 +1,18 @@
 package com.example.debatematch.domain.debatetag
 
+import com.example.debatematch.domain.debatetag.enum.SubjectTag
 import com.example.debatematch.global.base.BaseEntity
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Table
+import java.util.UUID
 
 @Entity
 @Table(name = "tbl_debate_tag")
 class DebateTag(
-    debateId: String,
-    tagId: String,
+    val debateId: UUID,
+    @Enumerated(EnumType.STRING)
+    val tag: SubjectTag,
 ): BaseEntity() {
 }
--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/debatetag/persistence/DebateTagRepository.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debatetag/persistence/DebateTagRepository.kt
@@ -1,8 +1,10 @@
 package com.example.debatematch.domain.debatetag.persistence
 
 import com.example.debatematch.domain.debatetag.DebateTag
+import com.example.debatematch.domain.debatetag.enum.SubjectTag
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface DebateTagRepository: JpaRepository<DebateTag, UUID> {
+    fun findAllByDebateId(debateId: UUID): List<DebateTag>
 }
