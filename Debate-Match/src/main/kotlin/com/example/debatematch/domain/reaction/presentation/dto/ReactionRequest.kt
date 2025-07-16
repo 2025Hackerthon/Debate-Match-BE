cf8f037095b1bc7832cae7a68f2f72eac04a141d
dohwi <dohwe0978@gmail.com>
2025-07-16 10:44:11 +0900

    feat :: reaction service

---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/reaction/presentation/dto/ReactionRequest.kt
@@ -0,0 +1,11 @@
+package com.example.debatematch.domain.reaction.presentation.dto
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import java.util.UUID
+
+data class ReactionRequest(
+    val reaction: DebateSide,
+    val debateId: UUID
+) {
+
+}
---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/reaction/serivce/ReactionService.kt
@@ -0,0 +1,28 @@
+package com.example.debatematch.domain.reaction.serivce
+
+import com.example.debatematch.domain.debate.enum.DebateStatus
+import com.example.debatematch.domain.debate.persistence.DebateRepository
+import com.example.debatematch.domain.reaction.Reaction
+import com.example.debatematch.domain.reaction.exception.InvalidDebateReactionException
+import com.example.debatematch.domain.reaction.presentation.dto.ReactionRequest
+import com.example.debatematch.domain.reaction.psersistence.ReactionRepository
+import com.example.debatematch.domain.user.facade.UserFacade
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+class ReactionService(
+    private val reactionRepository: ReactionRepository,
+    private val userFacade: UserFacade,
+    private val debateRepository: DebateRepository
+) {
+    @Transactional
+    fun execute(request: ReactionRequest){
+        val user = userFacade.currentUser()
+        val debate = debateRepository.findById(request.debateId).orElseThrow()
+        if(debate.status != DebateStatus.DONE) throw InvalidDebateReactionException
+
+        val reaction = if (reactionRepository.existsByDebateIdAndUserId(request.debateId, user.id!!)) reactionRepository.findByDebateIdAndUserId(request.debateId, user.id!!) else Reaction(debateId = request.debateId, userId = user.id!!, request.reaction)
+        reaction!!.update(request.reaction)
+    }
+}
