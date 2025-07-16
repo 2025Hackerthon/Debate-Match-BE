8c9db2da800ce4a66be1ea3d2e85599446e194d2
dohwi <dohwe0978@gmail.com>
2025-07-16 10:47:14 +0900

    feat :: debate

---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/presentation/dto/DebateCreateRequest.kt
@@ -0,0 +1,10 @@
+package com.example.debatematch.domain.debate.presentation.dto
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import com.example.debatematch.domain.debatetag.enum.SubjectTag
+
+data class DebateCreateRequest (
+    val tagList: List<SubjectTag>,
+    val title: String,
+    val side: DebateSide
+)
---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/service/DebateCreateService.kt
@@ -0,0 +1,37 @@
+package com.example.debatematch.domain.debate.service
+
+import com.example.debatematch.domain.debate.domain.Debate
+import com.example.debatematch.domain.debate.enum.DebateSide
+import com.example.debatematch.domain.debate.facade.DebateFacade
+import com.example.debatematch.domain.debate.persistence.DebateRepository
+import com.example.debatematch.domain.debate.presentation.dto.DebateCreateRequest
+import com.example.debatematch.domain.debatetag.DebateTag
+import com.example.debatematch.domain.debatetag.persistence.DebateTagRepository
+import com.example.debatematch.domain.participated.Participated
+import com.example.debatematch.domain.participated.persistence.ParticipatedRepository
+import com.example.debatematch.domain.user.facade.UserFacade
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class DebateCreateService(
+    private val debateRepository: DebateRepository,
+    private val debateTagRepository: DebateTagRepository,
+    private val userFacade: UserFacade,
+    private val participatedRepository: ParticipatedRepository
+) {
+    @Transactional
+    fun execute(request: DebateCreateRequest): UUID {
+        val user = userFacade.currentUser()
+
+        val debate = debateRepository.save(Debate(title = request.title))
+        val id = debate.id ?: throw IllegalStateException("Debate id is null")
+        request.tagList.map { debateTagRepository.save(DebateTag(debateId = id, tag = it)) }
+
+        participatedRepository.save(Participated(id, user.id!!, request.side))
+
+        return id
+
+    }
+}
