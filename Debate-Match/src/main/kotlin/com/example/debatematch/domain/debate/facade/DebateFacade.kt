6f02c28b5231525b79c90ad2f3a5c2076ee16688
dohwi <dohwe0978@gmail.com>
2025-07-16 10:46:24 +0900

    feat :: debate facade

---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/facade/DebateFacade.kt
@@ -0,0 +1,30 @@
+package com.example.debatematch.domain.debate.facade
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import java.util.*
+import kotlin.collections.HashMap
+
+@Component
+class DebateFacade {
+    companion object {
+        var eimotters: HashMap<String, SseEmitter> = HashMap()
+    }
+
+    fun getEmitterByDebateUuid(uuid: UUID, side: DebateSide): SseEmitter {
+        val key = uuid.toString()+side.name
+        println(key)
+        return eimotters[key] ?: throw RuntimeException("No eimotter for $uuid")
+    }
+
+    fun createEmitter(uuid: UUID, side: DebateSide): SseEmitter {
+        val key = uuid.toString()+side.name
+        val emitter = SseEmitter(Long.MAX_VALUE)
+        eimotters[key] = emitter
+        println(key)
+        return emitter
+    }
+
+
+}
