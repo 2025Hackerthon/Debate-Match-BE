5a885f22444992b07ecb77616aa59c5719fdc4f2
dohwi <dohwe0978@gmail.com>
2025-07-16 10:37:09 +0900

    feat :: debate persistence

--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/Debate.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/Debate.kt
@@ -1,40 +1,23 @@
 package com.example.debatematch.domain.debate.domain
 
-import com.example.debatematch.domain.debate.enum.DetadeStatus
+import com.example.debatematch.domain.debate.enum.DebateStatus
 import com.example.debatematch.global.base.BaseEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.persistence.Table
-import org.springframework.data.relational.core.mapping.Column
-import java.util.*
 
 @Entity
 @Table(name = "tbl_debate")
 class Debate (
     @Enumerated(EnumType.STRING)
-    var status: DetadeStatus,
+    var status: DebateStatus = DebateStatus.WAIT,
+    var summarize: String? = null,
+    val title: String
 
-    var introductionA: String,
 
-    var introductionB: String,
-
-    var rebuttalA: String,
-
-    var rebuttalB: String,
-
-    var defenseA: String,
-
-    var defenseB: String,
-
-    var conclusionA: String,
-
-    var conclusionB: String,
-
-    var summarize: String,
-
-    var userA: UUID,
-
-    var userB: UUID,
-
-    ): BaseEntity() {}
+): BaseEntity(){
+    fun updateToDone(){
+        status = DebateStatus.DONE
+    }
+}
--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/persistence/DebateRepository.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/persistence/DebateRepository.kt
@@ -1,8 +1,11 @@
 package com.example.debatematch.domain.debate.persistence
 
 import com.example.debatematch.domain.debate.domain.Debate
+import com.example.debatematch.domain.debate.enum.DebateStatus
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
+import java.util.*
 
 interface DebateRepository : JpaRepository<Debate, UUID> {
+    fun findAllByStatus(status: DebateStatus): List<Debate>
+    override fun findById(id: UUID): Optional<Debate>
 }
