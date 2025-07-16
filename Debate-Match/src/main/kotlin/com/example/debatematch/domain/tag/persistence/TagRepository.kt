ddad2c2b8685c18cc4b5c4ae37ece514fc157159
dohwi <dohwe0978@gmail.com>
2025-07-16 11:01:50 +0900

    delete :: tag

--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/tag/persistence/TagRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.debatematch.domain.tag.persistence
-
-import com.example.debatematch.domain.tag.Tag
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface TagRepository:JpaRepository<Tag,UUID> {
-}
