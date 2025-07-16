ee3f782bc1b65cb4fbd8f22e113453ba431110aa
dohwi <dohwe0978@gmail.com>
2025-07-16 10:47:36 +0900

    feat :: query

---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/presentation/dto/DebateQueryResponse.kt
@@ -0,0 +1,13 @@
+package com.example.debatematch.domain.debate.presentation.dto
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import com.example.debatematch.domain.debatetag.DebateTag
+import com.example.debatematch.domain.debatetag.enum.SubjectTag
+import java.util.UUID
+
+data class DebateQueryResponse(
+    val debateId: UUID,
+    val title: String,
+    val side: DebateSide,
+    val tags: List<SubjectTag>
+)
---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/service/DebateWaitingQueryAllService.kt
@@ -0,0 +1,32 @@
+package com.example.debatematch.domain.debate.service
+
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import com.example.debatematch.domain.debate.enum.DebateStatus
+import com.example.debatematch.domain.debate.persistence.DebateRepository
+import com.example.debatematch.domain.debate.presentation.dto.DebateQueryResponse
+import com.example.debatematch.domain.debatetag.persistence.DebateTagRepository
+import com.example.debatematch.domain.participated.persistence.ParticipatedRepository
+import com.example.debatematch.domain.reaction.psersistence.ReactionRepository
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+
+@Service
+class DebateWaitingQueryAllService(
+    private val debateRepository: DebateRepository,
+    private val reactionRepository: ReactionRepository,
+    private val debateTagRepository: DebateTagRepository,
+    private val participatedRepository: ParticipatedRepository
+) {
+    @Transactional
+    fun execute(): List<DebateQueryResponse> {
+        val debates = debateRepository.findAllByStatus(DebateStatus.WAIT)
+        return debates.map { DebateQueryResponse(title = it.title,
+            //pro = reactionRepository.countByDebateIdAndReaction(it.id!!,DebateSide.PRO),
+            tags = debateTagRepository.findAllByDebateId(it.id!!).map { it.tag },
+            debateId = it.id!!,
+            side = participatedRepository.findAllByDebateId(it.id!!)[0].side.changeSide()
+        ) }
+    }
+}
