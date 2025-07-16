00efffbb3e55d240b793ceef2752852f86a652ec
dohwi <dohwe0978@gmail.com>
2025-07-16 10:38:56 +0900

    feat :: reaction persistence

---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/reaction/Reaction.kt
@@ -0,0 +1,22 @@
+package com.example.debatematch.domain.reaction
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import com.example.debatematch.global.base.BaseEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "tbl_reaction")
+class Reaction(
+    val debateId: UUID,
+    val userId: UUID,
+    @Enumerated(EnumType.STRING)
+    var reaction: DebateSide
+): BaseEntity() {
+    fun update(side: DebateSide) {
+        this.reaction = side
+    }
+}
---
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/reaction/psersistence/ReactionRepository.kt
@@ -0,0 +1,13 @@
+package com.example.debatematch.domain.reaction.psersistence
+
+import com.example.debatematch.domain.debate.enum.DebateSide
+import com.example.debatematch.domain.reaction.Reaction
+import org.hibernate.metamodel.mapping.ForeignKeyDescriptor
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface ReactionRepository: JpaRepository<Reaction, UUID> {
+    fun countByDebateIdAndReaction(debateId: UUID, reaction: DebateSide): Int
+    fun findByDebateIdAndUserId(debateId: UUID, userId: UUID): Reaction?
+    fun existsByDebateIdAndUserId(debateId: UUID, userId: UUID): Boolean
+}
