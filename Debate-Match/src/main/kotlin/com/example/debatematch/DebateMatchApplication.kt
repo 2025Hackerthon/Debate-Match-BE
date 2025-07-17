81b9b2dcb182584de2577f5809b4c7d2d1b1fdde
dohwi <dohwe0978@gmail.com>
2025-07-17 09:21:32 +0900

    feat :: feign client 추가

--- Debate-Match/src/main/kotlin/com/example/debatematch/DebateMatchApplication.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/DebateMatchApplication.kt
@@ -3,13 +3,18 @@ package com.example.debatematch
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableFeignClients
 class DebateMatchApplication
 
 fun main(args: Array<String>) {
     runApplication<DebateMatchApplication>(*args)
 }
+
+
+
