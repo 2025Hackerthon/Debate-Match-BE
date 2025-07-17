0f55d23dc4ed22b4572f577d5416ae752e4d455b
dohwi <dohwe0978@gmail.com>
2025-07-17 09:18:03 +0900

    feat :: feign client

--- Debate-Match/build.gradle.kts
+++ Debate-Match/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "1.9.25"
-    id("org.jlleitschuh.gradle.ktlint").version("12.1.0")
+    kotlin("plugin.serialization") version "1.9.25" // Kotlin 버전에 맞게
 }
 
 group = "com.example"
@@ -16,6 +16,12 @@ java {
     }
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2025.0.0")
+    }
+}
+
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
@@ -24,6 +30,7 @@ configurations {
 
 repositories {
     mavenCentral()
+    google()
 }
 
 dependencies {
@@ -42,9 +49,12 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("javax.servlet:javax.servlet-api:4.0.1")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
-    implementation("javax.xml.bind:jaxb-api:2.3.1")
-    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.1")
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+    implementation ("javax.xml.bind:jaxb-api:2.3.1")
+    implementation ("org.glassfish.jaxb:jaxb-runtime:2.3.1")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
 }
 
 kotlin {
