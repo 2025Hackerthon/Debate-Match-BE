6112f78677272080540289216ae530733fadc5c5
dohwi <dohwe0978@gmail.com>
2025-07-17 09:26:18 +0900

    feat :: import추가

--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/presentation/dto/DebateDoneQueryAllResponse.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/presentation/dto/DebateDoneQueryAllResponse.kt
@@ -1,5 +1,6 @@
 package com.example.debatematch.domain.debate.presentation.dto
 
+import com.example.debatematch.domain.debate.enum.DebateSide
 import com.example.debatematch.domain.debatetag.enum.SubjectTag
 import java.util.UUID
 
