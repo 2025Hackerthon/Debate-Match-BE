5c36ff74f4f459a86ce7051c8786dd508becbd67
dohwi <dohwe0978@gmail.com>
2025-07-21 03:57:57 +0900

    fix :: 잘못된 로직 제거

--- Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/service/DebateCancelService.kt
+++ Debate-Match/src/main/kotlin/com/example/debatematch/domain/debate/service/DebateCancelService.kt
@@ -1,8 +1,6 @@
 package com.example.debatematch.domain.debate.service
 
 import com.example.debatematch.domain.argument.persistence.ArgumentRepository
-import com.example.debatematch.domain.debate.enum.DebateStatus
-import com.example.debatematch.domain.debate.exception.InvalidCancelArgumentException
 import com.example.debatematch.domain.debate.persistence.DebateRepository
 import com.example.debatematch.domain.debate.presentation.dto.DebateCancelRequest
 import org.springframework.stereotype.Service
@@ -16,12 +14,11 @@ class DebateCancelService(
 ) {
     @Transactional
     fun execute(debateId: UUID, request: DebateCancelRequest) {
-        if (argumentRepository.existsBySideAndLevelAndDebateId(side = request.side, level = request.level, debateId = debateId) ||
-            debateRepository.findById(debateId).orElseThrow().status == DebateStatus.PLAY
-        ) {
-            throw InvalidCancelArgumentException
-        }
-
+//        if (argumentRepository.existsBySideAndLevelAndDebateId(side = request.side, level = request.level, debateId = debateId) ||
+//            debateRepository.findById(debateId).orElseThrow().status == DebateStatus.PLAY
+//        ) {
+//            throw InvalidCancelArgumentException
+//        }
         argumentRepository.deleteByDebateIdAndLevelAndSide(debateId = debateId, level = request.level, side = request.side)
     }
 }
